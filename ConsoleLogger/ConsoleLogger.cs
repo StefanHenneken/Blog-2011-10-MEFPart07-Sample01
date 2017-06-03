1dab245a7290f1fd96f7ac05de7da03453e5f058
Stefan Henneken <stefan.henneken@live.com>
2017-06-03 23:29:52 +0200

    Add project files.

---
+++ ConsoleLogger/ConsoleLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Formatter;
+
+namespace Logger
+{
+    [Export]
+    public class ConsoleLogger
+    {
+        [Import]
+        private Func<FormatterBase> FormatterFunc { get; set; }
+
+        public void Log(string message)
+        {
+            string formattedString = FormatterFunc().Format(message);
+            Console.WriteLine(formattedString);
+        }
+    }
+}
---
+++ TextFormatter/Formatter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace Formatter
+{
+    public class FormatterBase
+    {
+        public virtual string Format(string message)
+        {
+            return message;
+        }
+    }
+
+    public class FormatterTimeStamp : FormatterBase
+    {
+        public override string Format(string message)
+        {
+            return string.Format("{0} - {1}", DateTime.Now.ToShortTimeString(), message);
+        }
+    }
+
+    public class FormatterDateTimeStamp : FormatterBase
+    {
+        public override string Format(string message)
+        {
+            return string.Format("{0} {1} - {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString(), message);
+        }
+    }
+
+    public class FormatterFactory
+    {
+        private static bool toogle;
+
+        [Export]
+        public FormatterBase GetTextFormatter()
+        {
+            // an dieser Stelle wird entschieden, welcher
+            // Formatter zurückgeliefert werden soll.
+            toogle = !toogle;
+            if (toogle)
+                return new FormatterTimeStamp();
+            else
+                return new FormatterDateTimeStamp();
+        }
+    }
+}
